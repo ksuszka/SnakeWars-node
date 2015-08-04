fc9ae5a2b406499b3aa9a0e1d244f1e350ae0275
Krzysztof Szabelski <kszabelski@gmail.com>
2015-08-05 01:08:35 +0200

    Startup snake logic implemented - just go randomly avoiding collisions

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var settings = {
-	login: "PL3"
+    login: "PL3"
 }
 
 var net = require('net');
 var snakeEngine = require("./snakeEngine");
+var _ = require('lodash');
 //var util = require('util');
 
 var client = new net.Socket();
 client.connect(9977, 'localhost', function() {
-	console.log('Connected');
+    console.log('Connected');
 });
 
 var waitForPlayerId = false;
@@ -16,54 +17,53 @@ var playerId;
 
 var buffer = '';
 client.on('data', function(data) {
-	//console.log('Received: ' + data);
-	if (data.toString().indexOf("\r\n") === -1){
-	//	console.log("partial data retrieved")
+    // console.log('Received: ' + data);
+    
+    // Processing chanks of data
+    if (data.toString().indexOf("\r\n") === -1){
+        buffer = buffer + data;
+        return;
+    }
 
-		buffer = buffer + data;
-		return;
-	}
+    if (buffer) {
+        data = buffer + data;
+        buffer = '';
+    }
+    
+    // Actuall communication with server
+    if (data.toString().indexOf('ID') > -1)
+    {
+        waitForPlayerId = true;
+        sendCommand(settings.login);
+    }
+    else if (waitForPlayerId)
+    {
+        console.log('Saveing playerId: ' + data);
+        playerId = _.trim(data.toString());
+        waitForPlayerId = false;
+    } 
+    else
+    {
+        try {
+            var gameBoardState = JSON.parse(data);
+            
+            //console.log('Processing: ', gameBoardState);
 
-	if (buffer) {
-	//	console.log("completing buffer")
-		data = buffer + data;
-		buffer = '';
-	}
-	else {
-//		console.log("full line received")
-	}
-	
-	if (data.toString().indexOf('ID') > -1)
-	{
-		waitForPlayerId = true;
-		sendCommand(settings.login);
-	}
-	else if (waitForPlayerId)
-	{
-		console.log('Saveing playerId: ' + data);
-		playerId = data.toString();
-		waitForPlayerId = false;
-	} 
-	else
-	{
-		try {
-			var gameBoardState = JSON.parse(data);
-
-			var engine = new snakeEngine(playerId);
-			var nextMove = engine.getNextMove(gameBoardState);
-			sendCommand(nextMove);
-		}
-		catch (err) {
-			console.log("Error: ", err);
-		}
-	}
+            var engine = new snakeEngine(playerId);
+            var nextMove = engine.getNextMove(gameBoardState);
+            sendCommand(nextMove);
+        }
+        catch (err) {
+            console.log("Error: ", err);
+        }
+    }
 });
 
 function sendCommand(command) {
-	console.log('Sending: ' + command);
-	client.write(command + '\r\n');
+    console.log('Sending: ' + command);
+    client.write(command + '\r\n');
 }
 
 client.on('close', function() {
-	console.log('Connection closed');
+    console.log('Connection closed');
 });
--- snakeEngine.js
+++ snakeEngine.js
@@ -1,9 +1,81 @@
+var _ = require('lodash');
+
 module.exports = function(mySnakeId) {
     this.getNextMove = function(gameBoardState) {
-    	if (gameBoardState.snakes[1].isAlive) {
-			return "STRAIGHT";
-		}
 
-		return "";
-	}
+        //===========================
+        // Your snake logic goes here
+        //===========================
+
+        var mySnake = getSnake(mySnakeId, gameBoardState);
+
+        if (mySnake.isAlive) {
+            var occupiedCells = getOccupiedCells(gameBoardState);
+
+            // Check possible moves in random order.
+            var moves = [
+                "RIGHT",
+                "LEFT",
+                "STRAIGHT"
+            ];
+
+            while (moves) {
+                // Select random move and remove it from availiable moves.
+                var index = getRandomInt(0, moves.length - 1);
+                var move = moves[index];
+                moves.splice(index, 1);
+
+                var newHead = getSnakeNewHeadPosition(mySnake, move);
+
+                console.log("test", _.some([{x: 1, y:2}, {x: 3, y:4}, {x: 4, y:5}, {x: 6, y:1}], function(p) {
+                    return p.x === 3 && p.y === 4;
+                }))
+
+                console.log("occuped", occupiedCells, "newhead", newHead)
+                var isOccuped = _.some(occupiedCells, function(p) {
+                    return p.x === newHead.x && p.y === newHead.y;
+                });
+
+                if (!isOccuped)
+                {
+                    return move;
+                }
+            }
+        }
+
+        return '';
+    }
+
+    var getSnake = function(snakeId, gameBoardState) {
+        return _.find(gameBoardState.snakes, { id: mySnakeId});
+    };
+
+    var getOccupiedCells = function(gameBoardState) {
+        var snakesCells =_.flatten(_.map(gameBoardState.snakes, function(snake) { return snake.cells}));
+        return _.union(gameBoardState.walls, snakesCells)
+    };
+
+    var offsets = {
+        Up: { RIGHT: {x: 1, y: 0}, LEFT: {x:-1, y:0}, STRAIGHT: {x: 0, y:1} },
+        Down: { RIGHT: {x: -1, y: 0}, LEFT: {x:1, y:0}, STRAIGHT: {x:0, y:-1} },
+        Left: { RIGHT: {x: 0, y: 1}, LEFT: {x:0, y:-1}, STRAIGHT: {x:-1, y:0} },
+        Right: { RIGHT: {x: 0, y: -1}, LEFT: {x:0, y:1}, STRAIGHT: {x:1, y:0} }
+    }
+
+    var getSnakeNewHeadPosition = function(mySnake, move) {
+        var head = mySnake.head;
+        var offsetsFromDir = offsets[mySnake.direction];
+        if (!offsetsFromDir)
+            return head;
+
+        var offset = offsetsFromDir[move];
+        if (!offset)
+            throw "Invalid move. Use only LEFT, RIGHT, STRAIGHT to calc snakeNewHeadPosition"
+
+        return {x : head.x + offset.x, y: head.y + offset.y };
+    }
+    
+    var getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 }
